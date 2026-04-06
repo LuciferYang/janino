27e776e000aff1bdef64af1176d9e9107eb1065e
github-action-benchmark <github@users.noreply.github.com>
2026-04-06 16:14:43 +0000

    add Benchmark (jmh) benchmark result for d14b7ce792e72e2a48403fe54f353beb177cb927

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775491872854,
+  "lastUpdate": 1775492083863,
   "repoUrl": "https://github.com/LuciferYang/janino",
   "entries": {
     "Benchmark": [
@@ -112,6 +112,120 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.codehaus.janino.benchmarks.SelfCompileBenchmark.compileScannerClass",
             "value": 19.76595975,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yangjie01@baidu.com",
+            "name": "yangjie01",
+            "username": "LuciferYang"
+          },
+          "committer": {
+            "email": "yangjie01@baidu.com",
+            "name": "yangjie01",
+            "username": "LuciferYang"
+          },
+          "distinct": true,
+          "id": "d14b7ce792e72e2a48403fe54f353beb177cb927",
+          "message": "fix: add ServicesResourceTransformer to shade plugin to fix SPI warnings\n\nMultiple JARs (janino, commons-compiler-jdk) contain\nMETA-INF/services/org.codehaus.commons.compiler.ICompilerFactory.\nWithout ServicesResourceTransformer, shade plugin warns about overlapping\nresources and only keeps one entry, potentially breaking ServiceLoader.",
+          "timestamp": "2026-04-06T23:55:00+08:00",
+          "tree_id": "cf806250acaf2f67d4f2db86f26f3a7ddc2607a5",
+          "url": "https://github.com/LuciferYang/janino/commit/d14b7ce792e72e2a48403fe54f353beb177cb927"
+        },
+        "date": 1775492082980,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.codehaus.janino.benchmarks.CompilationUnitBenchmark.compileComplexClass",
+            "value": 0.420463752084231,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.CompilationUnitBenchmark.compileSimpleClass",
+            "value": 0.06042599013659512,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ExpressionCompileBenchmark.compileComplexExpression",
+            "value": 0.15728758074550275,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ExpressionCompileBenchmark.compileSimpleArithmetic",
+            "value": 0.12227889462865701,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ExpressionCompileBenchmark.createFastEvaluator",
+            "value": 0.1646735828060486,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ExpressionCompileBenchmark.evaluatePreCompiled",
+            "value": 0.000007093876849207764,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.JaninoVsJdkBenchmark.compileExpression ( {\"compilerFactoryClassName\":\"org.codehaus.janino.CompilerFactory\"} )",
+            "value": 0.13862777821950517,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.JaninoVsJdkBenchmark.compileExpression ( {\"compilerFactoryClassName\":\"org.codehaus.commons.compiler.jdk.CompilerFactory\"} )",
+            "value": 6.377150166779988,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.JaninoVsJdkBenchmark.evaluateExpression ( {\"compilerFactoryClassName\":\"org.codehaus.janino.CompilerFactory\"} )",
+            "value": 0.000007505281185270795,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.JaninoVsJdkBenchmark.evaluateExpression ( {\"compilerFactoryClassName\":\"org.codehaus.commons.compiler.jdk.CompilerFactory\"} )",
+            "value": 0.000010339643643092438,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ScriptCompileBenchmark.compileLoopScript",
+            "value": 0.15117284092005664,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ScriptCompileBenchmark.compileSimpleScript",
+            "value": 0.11704804300453807,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.ScriptCompileBenchmark.executePreCompiled",
+            "value": 0.000024760725425680057,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.SelfCompileBenchmark.compileParserClass",
+            "value": 67.65676224999999,
+            "unit": "ms/op",
+            "extra": "iterations: 20\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "org.codehaus.janino.benchmarks.SelfCompileBenchmark.compileScannerClass",
+            "value": 20.89941765,
             "unit": "ms/op",
             "extra": "iterations: 20\nforks: 2\nthreads: 1"
           }
